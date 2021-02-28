0092701ced2df723b757b2f815ebfda1b45f00ab
SantamariaRamosJ <77521543+SantamariaRamosJ@users.noreply.github.com>
2021-02-28 16:32:22 -0400

    random system added

--- script.js
+++ script.js
@@ -10,6 +10,8 @@ function filterSelection(c) {
   }
 }
 
+ 
+
 function AddClass(element, name) {
   var i, arr1, arr2;
   arr1 = element.className.split(" ");
@@ -63,4 +65,17 @@ function myFunction() {
       li[i].style.display = "none";
     }
   }
+}
+
+
+function RandomCards(){
+  const cards = document.getElementsByClassName("arcanos");
+
+  const random = Math.floor(Math.random() * cards.length);
+  console.log(random);
+  console.log(cards[random]);
+
+  document.getElementById("imagen").appendChild(cards[random]);
+  console.log(document.getElementById("imagen"));
+
 }
